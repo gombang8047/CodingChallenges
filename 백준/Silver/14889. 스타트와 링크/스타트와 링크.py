9ac49c1bc2ff6ae12f25121216626449bd654c45
신지웅 <gombang8047@gmail.com>
2025-09-10 23:42:29 +0900

    [Silver I] Title: 스타트와 링크, Time: 3912 ms, Memory: 69844 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,12 +1,46 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+def calculate(n, lst, team):
+
+    if n >= N//2:
+        if len(process) == N//2:
+            team1.append(tuple(sorted(process)))
+            return
+
+    for i in range(N):
+        if used[i] == False:
+            if not process:
+                process.append(team[i])
+                used[i] = True
+                calculate(n+1, lst, team)
+                process.pop()
+                used[i] = False
+            elif team[i] > process[-1]:
+                process.append(team[i])
+                used[i] = True
+                calculate(n+1, lst, team)
+                process.pop()
+                used[i] = False
+
+N = int(input())
+result = []
+process = []
+lst = []
+for i in range(N):
+    lst.append(list(map(int, input().split())))
+team = [i for i in range(N)]
+used = [False] * N
+used_team = [False] * N
+
+team1 = []
+
+calculate(0, lst, team)
+
+fin = []
+
+team1 = list(set(team1))
+for k in range(len(team1)):
+    team2 = tuple(set([i for i in range(N)]) - set(team1[k]))
+    synergy1 = sum(lst[team1[k][i]][team1[k][j]] + lst[team1[k][j]][team1[k][i]] for i in range(len(team1[k])) for j in range(i+1, len(team1[k])))
+    synergy2 = sum(lst[team2[i]][team2[j]] + lst[team2[j]][team2[i]] for i in range(len(team2)) for j in range(i+1, len(team2)))
+    fin.append(abs(synergy1-synergy2))
+
+print(min(fin))
